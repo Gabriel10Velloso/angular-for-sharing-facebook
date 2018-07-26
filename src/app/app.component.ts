5d7002b195f6ae63d39768b98eb4b065f9669210
pullptong <hatsanai.d.tong@gmail.com>
2018-07-26 13:59:39 +0700

    [🎉] Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FacebookService, UIParams, UIResponse, InitParams, LoginResponse } from 'ngx-facebook';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(
+    private fb: FacebookService
+  ) {
+    const initParams: InitParams = {
+      appId: '245078872972696',
+      version: 'v3.0'
+    };
+
+    fb.init(initParams);
+  }
+
+  share() {
+    const params: UIParams = {
+      href: 'https://angular-for-seo.firebaseapp.com',
+      method: 'share'
+    };
+
+    this.fb.ui(params)
+      .then((res: UIResponse) => console.log(res))
+      .catch((e: any) => console.error(e));
+  }
+
+  shareWithOpenGraphActions() {
+    const params: UIParams = {
+      method: 'share_open_graph',
+      action_type: 'og.shares',
+      action_properties: JSON.stringify({
+          object: {
+              'og:url': 'https://angular-for-seo.firebaseapp.com',
+              'og:title': 'หัวข้ออันบั๊กเอ้ก',
+              'og:description': 'ข้อความยาว ๆ',
+              'og:image': 'https://futurism.com/wp-content/uploads/2017/09/download-600x315.png'
+          }
+      })
+    };
+
+    this.fb.ui(params)
+      .then((res: UIResponse) => console.log(res))
+      .catch((e: any) => console.error(e));
+  }
 }
